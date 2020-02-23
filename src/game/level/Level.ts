d89366d6c29068ee5e93e561d070ae374b1a0e13
Blaž Pečnik <blaz@easistent.com>
2020-02-23 17:53:27 +0100

    refactor: get light color

--- src/game/level/Level.ts
+++ src/game/level/Level.ts
@@ -10,14 +10,32 @@ import {
     Vector2,
     NearestFilter,
     Color,
-    VertexColors
+    VertexColors,
+    Box3
 } from "three";
 import { clamp } from "lodash";
 import { degToRad } from "../core/Utils";
 
+export interface LevelCell {
+    readonly index: number;
+
+    readonly x: number;
+    readonly y: number;
+    readonly z: number;
+
+    readonly ceil: boolean;
+    readonly wall: boolean;
+    readonly floor: boolean;
+    readonly ceilId: number;
+    readonly wallId: number;
+    readonly floorId: number;
+    readonly light: Color;
+    readonly aabb: Box3;
+}
+
 export class Level {
     public readonly scene = new Scene();
-    public readonly lightMap: Color[] = [];
+    public readonly cells: LevelCell[] = [];
     public rows = 0;
     public cols = 0;
 
@@ -35,21 +53,64 @@ export class Level {
             this.rows = tilemap.height;
             this.cols = tilemap.width;
 
-            this.buildLightMap(tilemap);
-            this.buildMesh(tilemap, tileset, texture);
+            this.buildCells(tilemap);
+            this.buildMesh(tileset, texture);
         });
     }
 
-    public getLightColor(x: number, y: number) {
+    public getCell(x: number, y: number): LevelCell | undefined {
         const index = y * this.cols + x;
-        return this.lightMap[index] || new Color(0x000000);
+        const cell = this.cells[index];
+        return cell;
     }
 
-    private buildMesh(
-        tilemap: Tiled2D.Tilemap,
-        tileset: Tiled2D.Tileset,
-        texture: Texture
-    ) {
+    public isWallCell(x: number, y: number) {
+        const cell = this.getCell(x, y);
+        return cell !== undefined && cell.wall;
+    }
+
+    private buildCells(tilemap: Tiled2D.Tilemap) {
+        const ceil = Tiled2D.getLayerTiles(tilemap, "Ceiling");
+        const wall = Tiled2D.getLayerTiles(tilemap, "Wall");
+        const floor = Tiled2D.getLayerTiles(tilemap, "Floor");
+        const lightMap = this.getLightMap(tilemap);
+        const length = this.rows * this.cols;
+        this.cells.length = 0;
+        for (let i = 0; i < length; i++) {
+            const cell: LevelCell = {
+                index: i,
+                x: i % this.cols,
+                y: 0,
+                z: Math.floor(i / this.cols),
+
+                ceilId: ceil[i],
+                wallId: wall[i],
+                floorId: floor[i],
+
+                ceil: ceil[i] > 0,
+                wall: wall[i] > 0,
+                floor: floor[i] > 0,
+
+                light: lightMap[i],
+                aabb: new Box3()
+            };
+
+            cell.aabb.min.x = cell.x - 0.5;
+            cell.aabb.min.y = cell.y - 0.5;
+            cell.aabb.min.z = cell.z - 0.5;
+
+            cell.aabb.max.x = cell.x + 0.5;
+            cell.aabb.max.y = cell.y + 0.5;
+            cell.aabb.max.z = cell.z + 0.5;
+
+            Object.freeze(cell.aabb.min);
+            Object.freeze(cell.aabb.max);
+
+            this.cells.push(cell);
+        }
+    }
+
+    private buildMesh(tileset: Tiled2D.Tileset, texture: Texture) {
         const setTextureUV = (cords: Vector2[][], tileId: number) => {
             // Initialize UV
             const tileU = tileset.tilewidth / tileset.imagewidth;
@@ -92,18 +153,16 @@ export class Level {
             return plane;
         };
 
-        const ceil = Tiled2D.getLayerTiles(tilemap, "Ceiling");
-        const wall = Tiled2D.getLayerTiles(tilemap, "Wall");
-        const floor = Tiled2D.getLayerTiles(tilemap, "Floor");
+        const getLight = (x: number, z: number) => {
+            const cell = this.getCell(x, z);
+            return cell ? cell.light : new Color(0x000000);
+        };
 
         const planes: PlaneGeometry[] = [];
-        const getLight = this.getLightColor.bind(this);
-        floor.forEach((_, index) => {
-            const x = index % this.cols;
-            const z = Math.floor(index / this.cols);
+        this.cells.forEach(cell => {
+            const { x, z, ceilId, wallId, floorId } = cell;
             const color = getLight(x, z);
 
-            const ceilId = ceil[index];
             if (ceilId > 0) {
                 const plane = createPlane(ceilId, color);
                 plane.rotateX(degToRad(90));
@@ -111,7 +170,6 @@ export class Level {
                 planes.push(plane);
             }
 
-            const floorId = floor[index];
             if (floorId > 0) {
                 const plane = createPlane(floorId, color);
                 plane.rotateX(degToRad(-90));
@@ -119,11 +177,9 @@ export class Level {
                 planes.push(plane);
             }
 
-            const wallId = wall[index];
             if (wallId > 0) {
                 // Front wall
-                const frontWallIndex = (z + 1) * tilemap.width + x;
-                if (!wall[frontWallIndex]) {
+                if (!this.isWallCell(x, z + 1)) {
                     const frontWall = createPlane(wallId, getLight(x, z + 1));
                     frontWall.rotateY(degToRad(0));
                     frontWall.translate(x, 0, z + 0.5);
@@ -131,8 +187,7 @@ export class Level {
                 }
 
                 // Back wall
-                const backWallIndex = (z - 1) * tilemap.width + x;
-                if (!wall[backWallIndex]) {
+                if (!this.isWallCell(x, z - 1)) {
                     const backWall = createPlane(wallId, getLight(x, z - 1));
                     backWall.rotateY(degToRad(180));
                     backWall.translate(x, 0, z - 0.5);
@@ -140,8 +195,7 @@ export class Level {
                 }
 
                 // Right wall
-                const rightWallIndex = z * tilemap.width + (x - 1);
-                if (!wall[rightWallIndex]) {
+                if (!this.isWallCell(x - 1, z)) {
                     const rightWall = createPlane(wallId, getLight(x - 1, z));
                     rightWall.rotateY(degToRad(-90));
                     rightWall.translate(x - 0.5, 0, z);
@@ -149,8 +203,7 @@ export class Level {
                 }
 
                 // Left wall
-                const leftWallIndex = z * tilemap.width + (x + 1);
-                if (!wall[leftWallIndex]) {
+                if (!this.isWallCell(x + 1, z)) {
                     const leftWall = createPlane(wallId, getLight(x + 1, z));
                     leftWall.rotateY(degToRad(90));
                     leftWall.translate(x + 0.5, 0, z);
@@ -175,7 +228,7 @@ export class Level {
         this.scene.add(levelMesh);
     }
 
-    private buildLightMap(tilemap: Tiled2D.Tilemap) {
+    private getLightMap(tilemap: Tiled2D.Tilemap) {
         const wallLayer = Tiled2D.getLayerTiles(tilemap, "Wall");
         const lightLayer = Tiled2D.getLayerTiles(tilemap, "Lights");
 
@@ -277,58 +330,56 @@ export class Level {
             }
         });
 
-        // Store new light map
-        this.lightMap.length = 0;
-        this.lightMap.push(...lightMap);
-
         // Render
         const renderDebug = true;
-        if (!renderDebug) return;
+        if (renderDebug) {
+            const canvas = document.createElement("canvas");
+            const tilesize = 8;
+            const padding = 0;
+            canvas.width = tilesize * this.cols;
+            canvas.height = tilesize * this.rows;
+            canvas.style.position = "absolute";
+            document.body.appendChild(canvas);
 
-        const canvas = document.createElement("canvas");
-        const tilesize = 8;
-        const padding = 0;
-        canvas.width = tilesize * this.cols;
-        canvas.height = tilesize * this.rows;
-        canvas.style.position = "absolute";
-        document.body.appendChild(canvas);
+            const ctx = canvas.getContext("2d");
+            if (ctx !== null) {
+                const fillTile = (x: number, y: number, color: string) => {
+                    ctx.fillStyle = color;
+                    ctx.fillRect(
+                        x * tilesize,
+                        y * tilesize,
+                        tilesize - padding,
+                        tilesize - padding
+                    );
+                };
 
-        const ctx = canvas.getContext("2d");
-        if (ctx !== null) {
-            const fillTile = (x: number, y: number, color: string) => {
-                ctx.fillStyle = color;
-                ctx.fillRect(
-                    x * tilesize,
-                    y * tilesize,
-                    tilesize - padding,
-                    tilesize - padding
-                );
-            };
+                ctx.fillStyle = "black";
+                ctx.fillRect(0, 0, 512, 512);
 
-            ctx.fillStyle = "black";
-            ctx.fillRect(0, 0, 512, 512);
+                for (let y = 0; y < tilemap.height; y++) {
+                    for (let x = 0; x < tilemap.width; x++) {
+                        const index = y * tilemap.width + x;
 
-            for (let y = 0; y < tilemap.height; y++) {
-                for (let x = 0; x < tilemap.width; x++) {
-                    const index = y * tilemap.width + x;
+                        const light = lightMap[index].clone();
+                        light.r = clamp(light.r, 0, 1);
+                        light.g = clamp(light.g, 0, 1);
+                        light.b = clamp(light.b, 0, 1);
+                        fillTile(x, y, "#" + light.getHexString());
 
-                    const light = lightMap[index].clone();
-                    light.r = clamp(light.r, 0, 1);
-                    light.g = clamp(light.g, 0, 1);
-                    light.b = clamp(light.b, 0, 1);
-                    fillTile(x, y, "#" + light.getHexString());
+                        const wallId = wallLayer[index];
+                        if (wallId > 0) {
+                            fillTile(x, y, "#008888");
+                        }
 
-                    const wallId = wallLayer[index];
-                    if (wallId > 0) {
-                        fillTile(x, y, "#008888");
-                    }
-
-                    const lightId = lightLayer[index];
-                    if (lightId > 0) {
-                        fillTile(x, y, "#aa2222");
+                        const lightId = lightLayer[index];
+                        if (lightId > 0) {
+                            fillTile(x, y, "#aa2222");
+                        }
                     }
                 }
             }
         }
+
+        return lightMap;
     }
 }
