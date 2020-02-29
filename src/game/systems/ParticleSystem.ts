f967329abee5447bec22a79af09d1de2a26edbe9
Blaž Pečnik <blaz@easistent.com>
2020-02-29 17:21:51 +0100

    feat: particle system

--- src/game/systems/ParticleSystem.ts
+++ src/game/systems/ParticleSystem.ts
@@ -3,6 +3,7 @@ import { World } from "../World";
 import { random } from "lodash";
 import { ParticleEmitterComponent, PositionComponent } from "../Components";
 import { Geometry, PointsMaterial, Vector3, Points } from "three";
+import { GRAVITY } from "../Globals";
 
 class Particle extends Vector3 {
     public readonly velocity = new Vector3();
@@ -25,7 +26,7 @@ export class ParticleSystem extends System {
             size: 1 / 32
         });
         this.particles = new Geometry();
-        for (let p = 0; p < 13; p++) {
+        for (let p = 0; p < 32; p++) {
             this.particles.vertices.push(new Particle(0, -2, 0));
         }
 
@@ -57,21 +58,21 @@ export class ParticleSystem extends System {
                 emitter.emitTime = world.elapsedTime;
                 emitter.count++;
 
-                for (let i = 0; i < 8; i++) {
+                for (let i = 0; i < 3; i++) {
                     const particle = this.getFreeParticle();
                     if (particle === undefined) continue;
 
                     const position = entity.getComponent(PositionComponent);
                     particle.set(position.x, position.y, position.z);
 
-                    const rand = 0.25;
+                    const rand = 0.5;
                     particle.velocity.copy(emitter.direction);
                     particle.velocity.x += random(-rand, rand, true);
                     particle.velocity.y += random(-rand, rand, true);
                     particle.velocity.z += random(-rand, rand, true);
 
                     particle.velocity.normalize();
-                    particle.velocity.multiplyScalar(0.05);
+                    particle.velocity.multiplyScalar(random(0.025, 0.05, true));
                 }
             }
         }
@@ -80,13 +81,11 @@ export class ParticleSystem extends System {
         for (let i = 0; i < this.particles.vertices.length; i++) {
             const particle = this.particles.vertices[i] as Particle;
             if (particle.y < 1 && particle.y > -1) {
-                this.particles.verticesNeedUpdate = true;
-                particle.velocity.multiplyScalar(0.9);
+                particle.velocity.x *= 0.9;
+                particle.velocity.z *= 0.9;
+                particle.velocity.y -= GRAVITY * dt * 0.01;
                 particle.add(particle.velocity);
-
-                if (particle.velocity.lengthSq() < 0.0001) {
-                    particle.y = -1000;
-                }
+                this.particles.verticesNeedUpdate = true;
             }
         }
     }
