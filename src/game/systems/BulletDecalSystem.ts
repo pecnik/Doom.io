3dcc4c73307658bf203690e8dd5c5990551c7acc
Blaž Pečnik <blaz@easistent.com>
2020-02-29 09:21:22 +0100

    refactor: bullet decal system

--- src/game/systems/BulletDecalSystem.ts
+++ src/game/systems/BulletDecalSystem.ts
@@ -12,27 +12,44 @@ import {
 } from "three";
 import { degToRad } from "../core/Utils";
 
+const PIXEL = 1 / 64;
+const DECAL_SIZE = PIXEL * 8;
+const snapToPixelgrid = (x: number) => {
+    return Math.round(x / PIXEL) * PIXEL;
+};
+
 export class BulletDecalSystem extends System {
     private readonly family: Family;
-    private readonly group: Group;
+    private readonly pool: Group;
 
     public constructor(world: World) {
         super();
-
-        this.group = new Group();
 
         this.family = new FamilyBuilder(world)
             .include(BulletDecalComponent)
             .include(PositionComponent)
             .build();
 
-        const PIXEL = 1 / 64;
-        const DECAL_SIZE = PIXEL * 8;
+        this.pool = this.createDecalMeshPool();
+        world.scene.add(this.pool);
+        world.addEntityListener({
+            onEntityAdded: (entity: Entity) => {
+                if (this.family.includesEntity(entity)) {
+                    this.removeOverlappingDecals(world, entity);
+                    this.spawnDecalMesh(world, entity);
+                }
+            },
+            onEntityRemoved: (entity: Entity) => {
+                if (this.family.includesEntity(entity)) {
+                    const decal = entity.getComponent(BulletDecalComponent);
+                    decal.mesh.visible = false;
+                }
+            }
+        });
+    }
 
-        const snapToPixelgrid = (x: number) => {
-            return Math.round(x / PIXEL) * PIXEL;
-        };
-
+    private createDecalMeshPool() {
+        const group = new Group();
         new TextureLoader().load("/assets/sprites/bullet_decal.png", map => {
             const geometry = new PlaneGeometry(
                 DECAL_SIZE,
@@ -41,7 +58,7 @@ export class BulletDecalSystem extends System {
             );
 
             // Fill pool
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 16; i++) {
                 const material = new MeshBasicMaterial({
                     map,
                     opacity: 1,
@@ -52,81 +69,68 @@ export class BulletDecalSystem extends System {
 
                 const mesh = new Mesh(geometry, material);
                 mesh.visible = false;
-                this.group.add(mesh);
+                group.add(mesh);
             }
         });
+        return group;
+    }
 
-        const onEntityAdded = (entity: Entity) => {
-            if (!this.family.includesEntity(entity)) {
-                return;
+    private removeOverlappingDecals(world: World, entity: Entity) {
+        // Remove overlapping decals
+        const overlappingDecals = [];
+        const vec1 = new Vector3();
+        const vec2 = new Vector3();
+        for (let i = 0; i < this.family.entities.length; i++) {
+            const peer = this.family.entities[i];
+            if (peer.id === entity.id) continue;
+
+            const pos1 = peer.getComponent(PositionComponent);
+            const pos2 = entity.getComponent(PositionComponent);
+            vec1.set(pos1.x, pos1.y, pos1.z);
+            vec2.set(pos2.x, pos2.y, pos2.z);
+            if (vec1.distanceToSquared(vec2) < DECAL_SIZE ** 2) {
+                overlappingDecals.push(peer);
             }
+        }
 
-            // Remove overlapping decals
-            const overlappingDecals = [];
-            const vec1 = new Vector3();
-            const vec2 = new Vector3();
-            for (let i = 0; i < this.family.entities.length; i++) {
-                const peer = this.family.entities[i];
-                if (peer.id === entity.id) continue;
+        world.removeEntities(...overlappingDecals);
+    }
 
-                const pos1 = peer.getComponent(PositionComponent);
-                const pos2 = entity.getComponent(PositionComponent);
-                vec1.set(pos1.x, pos1.y, pos1.z);
-                vec2.set(pos2.x, pos2.y, pos2.z);
-                if (vec1.distanceToSquared(vec2) < DECAL_SIZE ** 2) {
-                    overlappingDecals.push(peer);
-                }
-            }
+    private spawnDecalMesh(world: World, entity: Entity) {
+        const mesh = this.pool.children.find(m => !m.visible) as Mesh;
+        if (mesh === undefined) return;
 
-            world.removeEntities(...overlappingDecals);
+        // Init decal data
+        const decal = entity.getComponent(BulletDecalComponent);
+        decal.mesh = mesh;
+        decal.spawnTime = world.elapsedTime;
 
-            const mesh = this.group.children.find(m => !m.visible) as Mesh;
-            if (mesh === undefined) return;
+        // Reset mesh
+        mesh.visible = true;
+        mesh.rotation.set(0, 0, 0);
 
-            // Init decal data
-            const decal = entity.getComponent(BulletDecalComponent);
-            decal.mesh = mesh;
-            decal.spawnTime = world.elapsedTime;
+        // Clone position
+        const position = entity.getComponent(PositionComponent);
+        mesh.position.set(position.x, position.y, position.z);
 
-            // Reset mesh
-            mesh.visible = true;
-            mesh.rotation.set(0, 0, 0);
-
-            // Clone position
-            const position = entity.getComponent(PositionComponent);
-            mesh.position.set(position.x, position.y, position.z);
-
-            // Match wall surface
-            const offset = (1 / 1024) * decal.facing;
-            if (decal.axis === "x") {
-                mesh.rotation.y = degToRad(90) * decal.facing;
-                mesh.position.z = snapToPixelgrid(mesh.position.z);
-                mesh.position.y = snapToPixelgrid(mesh.position.y);
-                mesh.position.x += offset;
-            } else if (decal.axis === "y") {
-                mesh.rotation.x = degToRad(-90) * decal.facing;
-                mesh.position.x = snapToPixelgrid(mesh.position.x);
-                mesh.position.z = snapToPixelgrid(mesh.position.z);
-                mesh.position.y += offset;
-            } else if (decal.axis === "z") {
-                mesh.rotation.y = decal.facing === 1 ? 0 : degToRad(180);
-                mesh.position.x = snapToPixelgrid(mesh.position.x);
-                mesh.position.y = snapToPixelgrid(mesh.position.y);
-                mesh.position.z += offset;
-            }
-        };
-
-        const onEntityRemoved = (entity: Entity) => {
-            if (!this.family.includesEntity(entity)) {
-                return;
-            }
-
-            const decal = entity.getComponent(BulletDecalComponent);
-            decal.mesh.visible = false;
-        };
-
-        world.addEntityListener({ onEntityAdded, onEntityRemoved });
-        world.scene.add(this.group);
+        // Match wall surface
+        const offset = (1 / 1024) * decal.facing;
+        if (decal.axis === "x") {
+            mesh.rotation.y = degToRad(90) * decal.facing;
+            mesh.position.z = snapToPixelgrid(mesh.position.z);
+            mesh.position.y = snapToPixelgrid(mesh.position.y);
+            mesh.position.x += offset;
+        } else if (decal.axis === "y") {
+            mesh.rotation.x = degToRad(-90) * decal.facing;
+            mesh.position.x = snapToPixelgrid(mesh.position.x);
+            mesh.position.z = snapToPixelgrid(mesh.position.z);
+            mesh.position.y += offset;
+        } else if (decal.axis === "z") {
+            mesh.rotation.y = decal.facing === 1 ? 0 : degToRad(180);
+            mesh.position.x = snapToPixelgrid(mesh.position.x);
+            mesh.position.y = snapToPixelgrid(mesh.position.y);
+            mesh.position.z += offset;
+        }
     }
 
     public update(world: World) {
