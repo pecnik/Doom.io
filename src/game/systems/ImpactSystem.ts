29da87fd6008b4cfb1c58da0b54968a74a7264c8
Blaž Pečnik <blaz@easistent.com>
2020-02-29 07:53:48 +0100

    feat: bullet decals

--- src/game/systems/ImpactSystem.ts
+++ src/game/systems/ImpactSystem.ts
@@ -1,12 +1,21 @@
 import { System, Family, FamilyBuilder, Entity } from "@nova-engine/ecs";
 import { World } from "../World";
 import { PositionComponent, ImpactTag, NormalComponent } from "../Components";
-import { Group, Mesh, MeshBasicMaterial, PlaneGeometry } from "three";
+import {
+    Group,
+    Mesh,
+    MeshBasicMaterial,
+    PlaneGeometry,
+    TextureLoader,
+    NearestFilter,
+    MultiplyBlending
+} from "three";
 import { degToRad } from "../core/Utils";
 
 export class ImpactSystem extends System {
     private readonly family: Family;
     private readonly group: Group;
+    private ringBufferIndex = 0;
 
     public constructor(world: World) {
         super();
@@ -20,47 +29,79 @@ export class ImpactSystem extends System {
             .build();
 
         const pixel = 1 / 64;
-        const size = pixel * 2;
-        const geo = new PlaneGeometry(size, size, size);
-        const mat = new MeshBasicMaterial({ color: 0xffff00 });
+
+        new TextureLoader().load("/assets/sprites/bullet_decal.png", map => {
+            const size = pixel * 8;
+            const geometry = new PlaneGeometry(size, size, size);
+            const material = new MeshBasicMaterial({
+                map,
+                blending: MultiplyBlending
+            });
+            map.minFilter = NearestFilter;
+            map.magFilter = NearestFilter;
+
+            // Fill pool
+            for (let i = 0; i < 32; i++) {
+                const mesh = new Mesh(geometry, material);
+                mesh.visible = false;
+                this.group.add(mesh);
+            }
+        });
+
         const onEntityAdded = (entity: Entity) => {
             if (!this.family.includesEntity(entity)) {
                 return;
             }
 
+            this.ringBufferIndex += 1;
+            this.ringBufferIndex %= this.group.children.length - 1;
+
             const position = entity.getComponent(PositionComponent);
             const normal = entity.getComponent(NormalComponent);
 
-            const mesh = new Mesh(geo, mat);
+            const mesh = this.group.children[this.ringBufferIndex];
+            mesh.visible = true;
             mesh.position.set(position.x, position.y, position.z);
+            mesh.rotation.set(0, 0, 0);
 
-            const offset = 0.01;
+            const offset = (1 / 256) * this.ringBufferIndex;
             mesh.position.x += normal.x * offset;
             mesh.position.y += normal.y * offset;
             mesh.position.z += normal.z * offset;
 
+            // Rotate to match wall
             if (normal.x !== 0) {
                 mesh.rotation.y = degToRad(90) * normal.x;
-
-                mesh.position.z = Math.round(mesh.position.z / pixel) * pixel;
-                mesh.position.y = Math.round(mesh.position.y / pixel) * pixel;
             }
 
             if (normal.y !== 0) {
                 mesh.rotation.x = degToRad(-90) * normal.y;
-
-                mesh.position.x = Math.round(mesh.position.x / pixel) * pixel;
-                mesh.position.z = Math.round(mesh.position.z / pixel) * pixel;
             }
 
             if (normal.z === -1) {
                 mesh.rotation.y = degToRad(180);
-
-                mesh.position.x = Math.round(mesh.position.x / pixel) * pixel;
-                mesh.position.y = Math.round(mesh.position.y / pixel) * pixel;
             }
 
-            this.group.add(mesh);
+            // Fit to pixel grid
+            const fitToPixelGrid = true;
+            if (fitToPixelGrid) {
+                const round = (x: number) => Math.round(x / pixel) * pixel;
+
+                if (normal.x !== 0) {
+                    mesh.position.z = round(mesh.position.z);
+                    mesh.position.y = round(mesh.position.y);
+                }
+
+                if (normal.y !== 0) {
+                    mesh.position.x = round(mesh.position.x);
+                    mesh.position.z = round(mesh.position.z);
+                }
+
+                if (normal.z === -1) {
+                    mesh.position.x = round(mesh.position.x);
+                    mesh.position.y = round(mesh.position.y);
+                }
+            }
         };
 
         const onEntityRemoved = (entity: Entity) => {
