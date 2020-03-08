3cdee5c5cd7b18230db39b966234f9d8a874eaa6
Blaž Pečnik <blaz@easistent.com>
2020-03-08 08:08:05 +0100

    wip: pov gun sprite

--- src/game/systems/PovSystem.ts
+++ src/game/systems/PovSystem.ts
@@ -24,8 +24,28 @@ export class PovSystem extends System {
             map.minFilter = NearestFilter;
 
             const sprite = new Sprite(material);
+            sprite.renderOrder = 100;
 
             sprite.position.z = -4;
+            world.camera.add(sprite);
+        });
+
+        new TextureLoader().load("/assets/sprites/pov-gun.png", map => {
+            const material = new SpriteMaterial({
+                depthTest: false,
+                depthWrite: false,
+                map
+            });
+
+            map.magFilter = NearestFilter;
+            map.minFilter = NearestFilter;
+
+            const sprite = new Sprite(material);
+            sprite.renderOrder = 100;
+
+            sprite.position.x = 0.75;
+            sprite.position.y = -0.5;
+            sprite.position.z = -1;
             world.camera.add(sprite);
         });
     }
