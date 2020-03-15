465f5740351057d532c5961cc30792e7ff14f1b1
Blaž Pečnik <blaz@easistent.com>
2020-03-15 09:59:37 +0100

    feat: hitscan

--- src/game/data/EntityUtils.ts
+++ src/game/data/EntityUtils.ts
@@ -1,7 +1,15 @@
 import GLTFLoader from "three-gltf-loader";
 import { Entity, Family, EngineEntityListener } from "@nova-engine/ecs";
 import { Comp } from "./Comp";
-import { Mesh, MeshBasicMaterial, NearestFilter } from "three";
+import {
+    Mesh,
+    MeshBasicMaterial,
+    NearestFilter,
+    Raycaster,
+    PerspectiveCamera,
+    Vector2,
+    Intersection
+} from "three";
 import { World } from "./World";
 
 export function onFamilyChange(
@@ -65,4 +73,42 @@ export function setColliderFromMesh(entity: Entity) {
 
     collider.max.x = render.geo.boundingBox.max.x;
     collider.max.y = render.geo.boundingBox.max.z;
+}
+
+export module Hitscan {
+    export const caster: { entity?: Entity } = {};
+    export const raycaster = new Raycaster();
+    export const camera = new PerspectiveCamera(45);
+    export const origin = new Vector2();
+
+    const buffer = new Array<Intersection>();
+    const response: {
+        intersection?: Intersection;
+        entity?: Entity;
+    } = {};
+
+    export function cast(world: World, family?: Family) {
+        response.intersection = undefined;
+        response.entity = undefined;
+        buffer.length = 0;
+
+        const level = world.level.scene;
+        raycaster.intersectObject(level, true, buffer);
+        response.intersection = buffer[0];
+
+        if (family !== undefined) {
+            family.entities.forEach(entity => {
+                const render = entity.getComponent(Comp.Render);
+                raycaster.intersectObject(render.obj, true, buffer);
+
+                const [next] = buffer;
+                if (next !== undefined && next !== response.intersection) {
+                    response.intersection = next;
+                    response.entity = entity;
+                }
+            });
+        }
+
+        return response;
+    }
 }
--- src/game/systems/PlayerShootSystem.ts
+++ src/game/systems/PlayerShootSystem.ts
@@ -1,13 +1,18 @@
 import { System, Family, FamilyBuilder } from "@nova-engine/ecs";
 import { World } from "../data/World";
 import { Comp } from "../data/Comp";
+import { Hitscan } from "../data/EntityUtils";
 
 export class PlayerShootSystem extends System {
-    private readonly family: Family;
+    private readonly targets: Family;
+    private readonly shooters: Family;
 
     public constructor(world: World) {
         super();
-        this.family = new FamilyBuilder(world)
+
+        this.targets = new FamilyBuilder(world).include(Comp.Render).build();
+
+        this.shooters = new FamilyBuilder(world)
             .include(Comp.PlayerInput)
             .include(Comp.Position2D)
             .include(Comp.Rotation2D)
@@ -17,11 +22,11 @@ export class PlayerShootSystem extends System {
 
     public update(world: World) {
         const { elapsedTime } = world;
-        for (let i = 0; i < this.family.entities.length; i++) {
-            const entity = this.family.entities[i];
+        for (let i = 0; i < this.shooters.entities.length; i++) {
+            const entity = this.shooters.entities[i];
             const input = entity.getComponent(Comp.PlayerInput);
-            // const position = entity.getComponent(Comp.Position2D);
-            // const rotation = entity.getComponent(Comp.Rotation2D);
+            const position = entity.getComponent(Comp.Position2D);
+            const rotation = entity.getComponent(Comp.Rotation2D);
             const shooter = entity.getComponent(Comp.Shooter);
 
             const fireRate = 1 / 4;
@@ -31,9 +36,20 @@ export class PlayerShootSystem extends System {
 
                 console.log("> TODO: Play shoot sound");
 
-                console.log("> TODO: Hitscan level");
+                // Init hitscan
+                Hitscan.caster.entity = entity;
+                Hitscan.origin.set(0, 0);
+                Hitscan.camera.position.set(position.x, 0, position.y);
+                Hitscan.camera.rotation.set(rotation.x, rotation.y, 0, "YXZ");
+                Hitscan.camera.updateWorldMatrix(false, false);
+                Hitscan.raycaster.setFromCamera(Hitscan.origin, Hitscan.camera);
+                const rsp = Hitscan.cast(world, this.targets);
 
-                console.log("> TODO: Hitscan entity");
+                if (rsp.intersection === undefined) {
+                    return;
+                }
+
+                console.log({ rsp });
             }
         }
     }
