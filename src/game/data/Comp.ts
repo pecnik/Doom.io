c6f4ec920c5d3e4e784190f03beef89497bdad5d
Blaž Pečnik <blaz@easistent.com>
2020-03-20 22:41:11 +0100

    fix: component tags

--- src/game/data/Comp.ts
+++ src/game/data/Comp.ts
@@ -1,4 +1,5 @@
 import { Component } from "@nova-engine/ecs";
+import { uniqueId } from "lodash";
 import {
     Vector2,
     Object3D,
@@ -10,21 +11,33 @@ import {
 } from "three";
 
 export module Comp {
-    export class Position2D extends Vector2 implements Component {}
+    const NEXT_TAG = () => uniqueId("comp-tag");
 
-    export class Velocity2D extends Vector2 implements Component {}
+    export class Position2D extends Vector2 implements Component {
+        public static readonly tag = NEXT_TAG();
+    }
 
-    export class Rotation2D extends Vector2 implements Component {}
+    export class Velocity2D extends Vector2 implements Component {
+        public static readonly tag = NEXT_TAG();
+    }
 
-    export class Collider extends Box2 implements Component {}
+    export class Rotation2D extends Vector2 implements Component {
+        public static readonly tag = NEXT_TAG();
+    }
+
+    export class Collider extends Box2 implements Component {
+        public static readonly tag = NEXT_TAG();
+    }
 
     export class Collision implements Component {
+        public static readonly tag = NEXT_TAG();
         public readonly prev = new Vector2();
         public readonly next = new Vector2();
         public radius = 0.2;
     }
 
     export class PlayerInput implements Component {
+        public static readonly tag = NEXT_TAG();
         public movey = 0;
         public movex = 0;
         public lookHor = 0;
@@ -37,6 +50,7 @@ export module Comp {
     }
 
     export class Shooter implements Component {
+        public static readonly tag = NEXT_TAG();
         public swapTime = 0;
         public shootTime = 0;
         public weaponIndex = 0;
@@ -44,6 +58,7 @@ export module Comp {
     }
 
     export class Render implements Component {
+        public static readonly tag = NEXT_TAG();
         public static Geo: Geometry | BufferGeometry = new Geometry();
         public static Mat: MeshBasicMaterial = new MeshBasicMaterial();
         public obj = new Object3D();
@@ -52,15 +67,18 @@ export module Comp {
     }
 
     export class Footstep implements Component {
+        public static readonly tag = NEXT_TAG();
         public audio?: PositionalAudio;
     }
 
     export class Gunshot implements Component {
+        public static readonly tag = NEXT_TAG();
         public audio?: PositionalAudio;
         public origin = new Object3D();
     }
 
     export class Health implements Component {
+        public static readonly tag = NEXT_TAG();
         public value = 100;
     }
 }
