61cc7b64a7a4052d7cfd4c957a6dd210d76deee6
Blaž Pečnik <blaz@easistent.com>
2020-03-21 08:39:43 +0100

    refactor: hud element

--- src/game/systems/HudDisplaySystem.ts
+++ src/game/systems/HudDisplaySystem.ts
@@ -13,64 +13,48 @@ import {
 } from "three";
 import { HUD_WIDTH, HUD_HEIGHT } from "../data/Globals";
 
-export interface TextParams<T> {
-    readonly width: number;
-    readonly height: number;
-    readonly props: T;
-    readonly render: (ctx: CanvasRenderingContext2D) => void;
+export class HudElement<T> {
+    public readonly props: T;
+    public readonly plane: Mesh;
+    public readonly width: number;
+    public readonly height: number;
+
+    public readonly texture: Texture;
+    public readonly canvas: HTMLCanvasElement;
+    public readonly ctx: CanvasRenderingContext2D;
+
+    public constructor(config: { props: T; width: number; height: number }) {
+        const { props, width, height } = config;
+        this.props = props;
+        this.width = width;
+        this.height = height;
+
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = width;
+        this.canvas.height = height;
+
+        this.texture = new Texture(this.canvas);
+        this.texture.minFilter = NearestFilter;
+        this.texture.magFilter = NearestFilter;
+        this.texture.needsUpdate = true;
+
+        const material = new MeshBasicMaterial({ map: this.texture });
+        const geometry = new PlaneGeometry(width, height);
+        material.transparent = true;
+        this.plane = new Mesh(geometry, material);
+
+        this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    }
 }
 
 export class HudDisplaySystem extends System {
     private readonly family: Family;
-    private readonly ammoText = this.createText({
+
+    private readonly ammoText = new HudElement({
         width: 256,
         height: 128,
-        props: { loadedAmmo: 0 },
-        render: ctx => {
-            const { width, height } = this.ammoText;
-            const { loadedAmmo } = this.ammoText.props;
-
-            ctx.clearRect(0, 0, width, height);
-
-            ctx.fillStyle = "white";
-            ctx.fillText(loadedAmmo.toString(), width / 2, height / 2);
-        }
+        props: { loadedAmmo: 0 }
     });
-
-    private createText<T>(config: TextParams<T>) {
-        const { width, height, props, render } = config;
-
-        const canvas = document.createElement("canvas");
-        canvas.width = width;
-        canvas.height = height;
-
-        const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-        ctx.font = "Normal 40px Arial";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-
-        const texture = new Texture(canvas);
-        texture.minFilter = NearestFilter;
-        texture.magFilter = NearestFilter;
-        texture.needsUpdate = true;
-
-        const material = new MeshBasicMaterial({ map: texture });
-        const geometry = new PlaneGeometry(width, height);
-        material.transparent = true;
-
-        const plane = new Mesh(geometry, material);
-
-        return {
-            width,
-            height,
-            plane,
-            props,
-            render: () => {
-                render(ctx);
-                texture.needsUpdate = true;
-            }
-        };
-    }
 
     public constructor(world: World, hud: Hud) {
         super();
@@ -80,13 +64,20 @@ export class HudDisplaySystem extends System {
             .include(Comp.Shooter)
             .build();
 
+        // Ammo text
+        hud.scene.add(this.ammoText.plane);
+
+        this.ammoText.ctx.font = "Normal 40px Arial";
+        this.ammoText.ctx.textAlign = "center";
+        this.ammoText.ctx.textBaseline = "middle";
+        this.ammoText.ctx.fillStyle = "white";
+
         this.ammoText.plane.renderOrder = 100;
         this.ammoText.plane.position.set(
             HUD_WIDTH / 2 - this.ammoText.width / 2,
             -(HUD_HEIGHT / 2 - this.ammoText.height / 2),
             0
         );
-        hud.scene.add(this.ammoText.plane);
 
         // Load crosshair srpite
         new TextureLoader().load("/assets/sprites/crosshair.png", map => {
@@ -111,7 +102,14 @@ export class HudDisplaySystem extends System {
 
             if (this.ammoText.props.loadedAmmo !== shooter.loadedAmmo) {
                 this.ammoText.props.loadedAmmo = shooter.loadedAmmo;
-                this.ammoText.render();
+                const { ctx, width, height, props, texture } = this.ammoText;
+                texture.needsUpdate = true;
+                ctx.clearRect(0, 0, width, height);
+                ctx.fillText(
+                    props.loadedAmmo.toString(),
+                    width / 2,
+                    height / 2
+                );
             }
         }
     }
