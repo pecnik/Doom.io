bc2e06d2e2813f66c0e338b3b3df8e7b18df61e5
Blaž Pečnik <blaz@easistent.com>
2020-03-25 16:52:33 +0100

    feat: plane

--- src/editor/GameEditor.ts
+++ src/editor/GameEditor.ts
@@ -1,13 +1,10 @@
 import { Game } from "../game/core/Engine";
 import { Input } from "../game/core/Input";
-import { Scene, PerspectiveCamera } from "three";
+import { GameEditorWorld } from "./GameEditorWorld";
 
 export class GameEditor implements Game {
     public readonly input = new Input({ requestPointerLock: true });
-    public readonly world = {
-        scene: new Scene(),
-        camera: new PerspectiveCamera(90)
-    };
+    public readonly world = new GameEditorWorld();
 
     public preload(): Promise<any> {
         return Promise.resolve();
@@ -15,6 +12,7 @@ export class GameEditor implements Game {
 
     public create() {
         console.log(`> Editor::created`);
+        console.log(this.world);
     }
 
     public update() {
---
+++ src/editor/GameEditorWorld.ts
@@ -0,0 +1,38 @@
+import {
+    Scene,
+    PerspectiveCamera,
+    MeshBasicMaterial,
+    Mesh,
+    PlaneGeometry
+} from "three";
+
+export class GameEditorWorld {
+    public readonly width = 16;
+    public readonly depth = 16;
+
+    public readonly scene = new Scene();
+    public readonly camera = new PerspectiveCamera(90);
+    public readonly floor = this.createFloor();
+
+    public constructor() {
+        this.camera.position.z = 5;
+        this.camera.position.y = 2;
+
+        this.scene.add(this.floor);
+    }
+
+    private createFloor() {
+        const mat = new MeshBasicMaterial({ color: 0xff00ff, wireframe: true });
+        const geo = new PlaneGeometry(
+            this.width,
+            this.depth,
+            this.width,
+            this.depth
+        );
+
+        const floor = new Mesh(geo, mat);
+        floor.rotateX(Math.PI / 2);
+
+        return floor;
+    }
+}
