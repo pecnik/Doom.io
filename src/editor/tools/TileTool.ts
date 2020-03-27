1a2f1343ccac23068826f92b6f907a1a7c51dba6
Blaž Pečnik <blaz@easistent.com>
2020-03-27 08:12:12 +0100

    feat: texture select

--- src/editor/tools/TileTool.ts
+++ src/editor/tools/TileTool.ts
@@ -2,6 +2,8 @@ import { Tool } from "./Tool";
 import { KeyCode } from "../../game/core/Input";
 import { Intersection } from "three";
 import { Hitscan } from "../../game/utils/EntityUtils";
+import { HUD_WIDTH, HUD_HEIGHT } from "../../game/data/Globals";
+import { TILE_COLS, TILE_W, TILE_ROWS } from "../data/Constants";
 
 export class TileTool extends Tool {
     public readonly title = "Tile tool";
@@ -13,16 +15,26 @@ export class TileTool extends Tool {
     }
 
     public end() {
-        this.hud.texture.visible = false;
-        this.hud.cursor.position.setScalar(0);
-
         // Hitscan
+        const cursor = this.hud.cursor.position;
         const buffer: Intersection[] = [];
-        Hitscan.origin.set(0, 0);
+        Hitscan.origin.set(cursor.x / (HUD_WIDTH / 2), cursor.y / (HUD_HEIGHT / 2));
         Hitscan.raycaster.setFromCamera(Hitscan.origin, this.hud.camera);
         Hitscan.raycaster.intersectObject(this.hud.texture, true, buffer);
 
-        console.log({ buffer });
+        for (let i = 0; i < buffer.length; i++) {
+            const rsp = buffer[i];
+            if (rsp.uv) {
+                const x = Math.floor(rsp.uv.x * TILE_COLS);
+                const y = Math.floor((1 - rsp.uv.y) * TILE_ROWS);
+                const index = y * TILE_COLS + x;
+                this.world.texutreIndex = index + 1;
+            }
+        }
+
+        // Clear hud cursor
+        this.hud.texture.visible = false;
+        this.hud.cursor.position.setScalar(0);
     }
 
     public update() {
