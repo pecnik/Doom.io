391e50728e9f3e788f98e6a610a37cf85158c895
Blaž Pečnik <blaz@easistent.com>
2020-03-27 09:34:54 +0100

    refactor: hitscan

--- src/editor/tools/BlockTool.ts
+++ src/editor/tools/BlockTool.ts
@@ -16,48 +16,39 @@ export class BlockTool extends Tool {
     }
 
     private placeVoxel() {
-        const [hit] = this.hitscan();
-        if (!hit) return;
-        if (!hit.face) return;
-
-        const point = hit.point.clone();
-        const normal = hit.face.normal.clone().multiplyScalar(0.1);
-        point.add(normal);
-
-        const voxel = this.world.level.getVoxel(point);
+        const voxel = this.getVoxel(1);
         if (voxel !== undefined) {
             voxel.solid = true;
-
             for (let i = 0; i < 6; i++) {
                 voxel.faces[i] = this.world.texutreIndex;
             }
-
             buildLevelMesh(this.world.level);
         }
     }
 
     private removeVoxel() {
-        const [hit] = this.hitscan();
-        if (!hit) return;
-        if (!hit.face) return;
-
-        const point = hit.point.clone();
-        const normal = hit.face.normal.clone().multiplyScalar(0.1);
-        point.sub(normal);
-
-        const voxel = this.world.level.getVoxel(point);
+        const voxel = this.getVoxel(-1);
         if (voxel !== undefined) {
             voxel.solid = false;
             buildLevelMesh(this.world.level);
         }
     }
 
-    private hitscan() {
+    private getVoxel(dir: 1 | -1) {
         const buffer: Intersection[] = [];
         Hitscan.origin.set(0, 0);
         Hitscan.raycaster.setFromCamera(Hitscan.origin, this.world.camera);
         Hitscan.raycaster.intersectObject(this.world.floor, true, buffer);
         Hitscan.raycaster.intersectObject(this.world.level.scene, true, buffer);
-        return buffer;
+
+        const [hit] = buffer;
+        if (!hit) return;
+        if (!hit.face) return;
+
+        const point = hit.point.clone();
+        const normal = hit.face.normal.clone().multiplyScalar(0.1 * dir);
+        point.add(normal);
+
+        return this.world.level.getVoxel(point);
     }
 }
