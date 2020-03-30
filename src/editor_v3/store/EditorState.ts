6c7e518b10566bd1b22975b2a06ae8954e2bfcc8
Blaž Pečnik <blaz@easistent.com>
2020-03-30 18:37:58 +0200

    ...

--- src/editor_v3/store/EditorState.ts
+++ src/editor_v3/store/EditorState.ts
@@ -4,5 +4,5 @@ export class EditorState {
     public level = createLevel(0, 0, 0);
 
     public tileIdSlotIndex = 0;
-    public tileIdSlotArray = [0, 1, 2, 3, 4, 5, 6, 7];
+    public tileIdSlotArray = [0, 1, 2, 3, 4, 5];
 }
