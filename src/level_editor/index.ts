5fc0b789afbf77cd8c1b27c4ce8bfcf8b8f00348
Blaž Pečnik <blaz@easistent.com>
2020-04-02 18:40:38 +0200

    removed junk code

--- src/level_editor/index.ts
+++
@@ -1,8 +0,0 @@
-import { Engine } from "../game/core/Engine";
-import { Editor, VIEW_WIDTH, VIEW_HEIGHT } from "./Editor";
-
-new Engine(
-    new Editor(),
-    document.getElementById("viewport") as HTMLCanvasElement,
-    document.getElementById("gamearea") as HTMLDivElement
-).start(VIEW_WIDTH, VIEW_HEIGHT);
--- src/www/index.ts
+++ src/www/index.ts
@@ -2,10 +2,6 @@ const route = location.hash.replace("#", "");
 
 switch (route) {
     case "editor":
-        import("../level_editor/index");
-        break;
-
-    case "editor_v3":
         import("../editor_v3/index");
         break;
 
