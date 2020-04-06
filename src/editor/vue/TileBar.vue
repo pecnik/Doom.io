0b8d924ed3175a2bf8cb714452bb54330c6aa0f3
Blaž Pečnik <blaz@easistent.com>
2020-04-06 18:28:51 +0200

    feat: select texture

--- src/editor/vue/TileBar.vue
+++ src/editor/vue/TileBar.vue
@@ -1,11 +1,32 @@
 <template>
-    <div class="tile-bar">
-        <tile v-for="(tileId, index) in tileIdSlotArray"
-            class="tile"
-            @click.native="openTileSelectDialog(index)"
-            :key="index + 1"
-            :class="{ active: index === tileIdSlotIndex }"
-            :tileId="tileId"></tile>
+    <div>
+        <div class="tile-bar">
+            <tile v-for="(tileId, index) in tileIdSlotArray"
+                class="tile"
+                @click.native="openTileSelectDialog(index)"
+                :key="index + 1"
+                :class="{ active: index === tileIdSlotIndex }"
+                :tileId="tileId"></tile>
+        </div>
+        <v-dialog v-model="dialog" max-width="80vw">
+            <v-card>
+                <v-card-title class="headline">Select texture</v-card-title>
+                <v-card-text>
+                    <div class="tile-select-dialog">
+                        <tile v-for="(key, tileId) in 64" :key="key"
+                            @click.native="setTileSlot(tileId)"
+                            class="tile"
+                            :class="{ active: tileId === dialogTileId }"
+                            :tileId="tileId"></tile>
+                    </div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" text @click="dialog = false">Close</v-btn>
+                    <v-btn color="green darken-1" text @click="confirm">Select</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 <script>
@@ -22,9 +43,27 @@ export default {
     },
     methods: {
         openTileSelectDialog(index) {
-            this.$store.dispatch("setTileIdSlotIndex", index);
-            this.$store.dispatch("setTileSelectDialog", true);
+            this.dialog = true;
+            this.dialogIndex = index;
+            this.dialogTileId = this.$store.getters.activeTileId;
+        },
+        setTileSlot(tileId) {
+            this.dialogTileId = tileId;
+        },
+        confirm() {
+            const index = this.dialogIndex;
+            const tileId = this.dialogTileId;
+            this.$store.dispatch("setTileIdSlot", { index, tileId });
+            this.$store.dispatch("setTileSelectDialog", close);
+            this.dialog = false;
         }
+    },
+    data() {
+        return {
+            dialog: false,
+            dialogIndex: -1,
+            dialogTileId: -1
+        };
     }
 };
 </script>
