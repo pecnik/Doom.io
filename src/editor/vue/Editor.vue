def1e9bbbc9f7dcd55f9be45b6b5698fab2f2ce6
Blaž Pečnik <blaz@easistent.com>
2020-04-06 17:04:40 +0200

    layout fix

--- src/editor/vue/Editor.vue
+++ src/editor/vue/Editor.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
         <v-app>
-            <v-content>
-                <div ref="viewport"></div>
-            </v-content>
+        <div id="editor-app">
+            <div id="editor-header">HEADER</div>
+            <div id="editor-sidemenu">SIDEMENI</div>
+            <div id="editor-viewport" ref="viewport"></div>
+        </div>
         </v-app>
     </div>
 </template>
 <script>
 import { WebGLRenderer } from "three";
 import { Editor } from "../Editor";
+import { debounce } from "lodash";
+
 export default {
     mounted() {
         const viewport = this.$refs["viewport"];
@@ -19,9 +23,9 @@ export default {
         const editor = new Editor(renderer.domElement);
 
         let aspect = 0;
-        const onWindowResize = () => {
-            const width = window.innerWidth * 0.5;
-            const height = window.innerHeight * 0.5;
+        const onWindowResize = debounce(() => {
+            const width = viewport.offsetWidth;
+            const height = viewport.offsetHeight;
             const aspect = width / height;
             const camera = editor.world.camera;
             camera.aspect = aspect;
@@ -29,7 +33,7 @@ export default {
             camera.far = 1000;
             camera.updateProjectionMatrix();
             renderer.setSize(width, height);
-        };
+        }, 250);
 
         window.addEventListener("resize", onWindowResize);
         onWindowResize();
@@ -49,3 +53,34 @@ export default {
     }
 };
 </script>
+<style lang="scss">
+#editor-app {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    box-sizing: border-box;
+
+    display: grid;
+    grid-template-rows: 96px minmax(0, 1fr);
+    grid-template-columns: 250px minmax(0, 1fr);
+
+    #editor-header {
+        box-sizing: border-box;
+        grid-row: 1;
+        grid-column-start: 1;
+        grid-column-end: 3;
+    }
+
+    #editor-sidemenu {
+        box-sizing: border-box;
+        grid-row: 2;
+        grid-column: 1;
+    }
+
+    #editor-viewport {
+        box-sizing: border-box;
+        grid-row: 2;
+        grid-column: 2;
+    }
+}
+</style>
