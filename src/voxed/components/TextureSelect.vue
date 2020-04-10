b36872f24e2c83187dd11e2390519f8cf7b6d9b5
Blaž Pečnik <blaz@easistent.com>
2020-04-10 18:13:26 +0200

    open dialog

--- src/voxed/components/TextureSelect.vue
+++ src/voxed/components/TextureSelect.vue
@@ -1,7 +1,8 @@
 <style scoped lang="scss">
 @import "~vuetify/src/styles/main.sass";
 
-.texture-option {
+.texture-tile {
+    position: relative;
     display: inline-block;
     margin: 4px;
     padding: 4px;
@@ -17,17 +18,37 @@
     &.active {
         background-color: map-get($teal, "lighten-3");
     }
+
+    .texture-dialog-toggle {
+        display: none;
+    }
+
+    &:hover > .texture-dialog-toggle {
+        position: absolute;
+        bottom: 6px;
+        right: 6px;
+        display: block;
+        width: 24px;
+        height: 24px;
+        text-align: center;
+        line-height: 24px;
+        background-color: map-get($teal, "darken-3");
+    }
 }
 </style>
 <template>
     <div>
+
         <div v-for="(tileId, key) in slots"
             :key="key + 1"
             :class="{ active: key === index }"
-            @click="onSlotClick(key)"
-            class="texture-option">
-            <texture :tileId="tileId"></texture>
+            class="texture-tile">
+            <div @click="openTexutreDialog(key)" class="texture-dialog-toggle">
+                <v-icon size="12px">mdi-pencil</v-icon>
+            </div>
+            <texture :tileId="tileId" @click.native="onSlotClick(key)"></texture>
         </div>
+
          <v-dialog v-model="dialog.open" max-width="550px">
             <v-card>
                 <v-card-title class="headline">Select texture</v-card-title>
@@ -36,7 +57,7 @@
                         <div v-for="(key, tileId) in 64" :key="key"
                             :class="{ active: tileId === dialog.tileId }"
                             @click="selectDialogTexture(tileId)"
-                            class="texture-option">
+                            class="texture-tile">
                             <texture :tileId="tileId"></texture>
                         </div>
                     </div>
@@ -60,18 +81,21 @@ export default {
     methods: {
         onSlotClick(index) {
             this.$store.state.texture.index = index;
+        },
+        openTexutreDialog(index) {
             this.dialog.tileId = this.slots[index];
+            this.dialog.index = index;
             this.dialog.open = true;
         },
         selectDialogTexture(tileId) {
             const { texture } = this.$store.state;
-            texture.slots[texture.index] = tileId;
+            texture.slots[this.dialog.index] = tileId;
             this.dialog.open = false;
         }
     },
     data() {
         return {
-            dialog: { open: false, tileId: 0 }
+            dialog: { open: false, index: 0, tileId: 0 }
         };
     }
 };
