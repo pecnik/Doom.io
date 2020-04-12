01903bd957e9af4f5881c6f5e5e9fc3c4ff423be
Blaž Pečnik <blaz@easistent.com>
2020-04-12 08:12:07 +0200

    ...

--- src/voxed/App.vue
+++ src/voxed/App.vue
@@ -1,91 +1,25 @@
-<style scoped lang="scss">
-#voxed-app {
-    width: 100vw;
-    height: 100vh;
-    display: grid;
-    grid-template-rows: minmax(0, 1fr);
-    grid-template-columns: minmax(0, 1fr) 300px;
-    box-sizing: border-box;
-
-    #voxed-sidemenu {
-        grid-row: 1;
-        grid-column: 2;
-        padding: 16px;
-    }
-
-    #voxed-viewport {
-        grid-row: 1;
-        grid-column: 1;
-    }
-
-    #voxed-viewport-cursor {
-        grid-row: 1;
-        grid-column: 1;
-        pointer-events: none;
-
-        /* center fa curosr */
-        display: grid;
-        align-content: space-evenly;
-        justify-content: space-evenly;
-
-        .fa {
-            $cursor_size: 24px;
-            pointer-events: none;
-            width: $cursor_size;
-            height: $cursor_size;
-            border-radius: $cursor_size / 2;
-
-            opacity: 0.75;
-            color: #000;
-            background: #fff;
-
-            text-align: center;
-            font-size: $cursor_size / 2;
-            line-height: $cursor_size;
-        }
-    }
-}
-</style>
 <template>
     <v-app>
         <div id="voxed-app">
-
-            <!-- Viewport -->
             <div id="voxed-viewport" ref="viewport"></div>
             <div id="voxed-viewport-cursor">
                 <i class="fa" :class="toolIcon"></i>
             </div>
-
-            <!-- Side menu -->
             <div id="voxed-sidemenu" ref="sidemenu">
-                <v-btn x-large block @click="play">PLAY</v-btn>
-                <br>
-                <br>
-
-                <v-card class="mb-4">
-                    <v-card-text>
-                        <tool-select></tool-select>
-                    </v-card-text>
-                    <v-card-text>
-                        <render-settings></render-settings>
-                    </v-card-text>
-                </v-card>
-                <texture-select></texture-select>
+                <v-btn class="mb-8" x-large block @click="play">PLAY</v-btn>
+                <sidemenu></sidemenu>
             </div>
         </div>
     </v-app>
 </template>
 <script>
-import DefaultLevel from "../../assets/levels/level_one.json";
-import ToolSelect from "./components/ToolSelect.vue";
-import TextureSelect from "./components/TextureSelect.vue";
-import RenderSettings from "./components/RenderSettings.vue";
+import Sidemenu from "./components/Sidemenu.vue";
 import { Editor } from "./Editor";
 
 const editor = Editor.getInstance();
 
 export default {
-    components: { ToolSelect, RenderSettings, TextureSelect },
+    components: { Sidemenu },
     computed: {
         toolIcon() {
             const { toolId } = this.$store.state;
@@ -138,3 +72,51 @@ export default {
     }
 };
 </script>
+<style scoped lang="scss">
+#voxed-app {
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-rows: minmax(0, 1fr);
+    grid-template-columns: minmax(0, 1fr) 400px;
+    box-sizing: border-box;
+
+    #voxed-sidemenu {
+        grid-row: 1;
+        grid-column: 2;
+        padding: 16px;
+    }
+
+    #voxed-viewport {
+        grid-row: 1;
+        grid-column: 1;
+    }
+
+    #voxed-viewport-cursor {
+        grid-row: 1;
+        grid-column: 1;
+        pointer-events: none;
+
+        /* center fa curosr */
+        display: grid;
+        align-content: space-evenly;
+        justify-content: space-evenly;
+
+        .fa {
+            $cursor_size: 24px;
+            pointer-events: none;
+            width: $cursor_size;
+            height: $cursor_size;
+            border-radius: $cursor_size / 2;
+
+            opacity: 0.75;
+            color: #000;
+            background: #fff;
+
+            text-align: center;
+            font-size: $cursor_size / 2;
+            line-height: $cursor_size;
+        }
+    }
+}
+</style>
---
+++ src/voxed/components/Sidemenu.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-tabs v-model="tab">
+            <v-tab  key="block">Block</v-tab>
+            <v-tab key="texture">Texture</v-tab>
+            <v-tab key="texture">Settings</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+            <v-tab-item key="0">
+                <v-card flat>
+                    <v-card-text>
+                        <sidemenu-block></sidemenu-block>
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+            <v-tab-item key="1">
+                <v-card flat>
+                    <v-card-text>
+                        <sidemenu-texture></sidemenu-texture>
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+             <v-tab-item key="2">
+                <v-card flat>
+                    <v-card-text>
+                        <sidemenu-settings></sidemenu-settings>
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+        </v-tabs-items>
+    </div>
+</template>
+<script>
+import SidemenuBlock from "./SidemenuBlock.vue";
+import SidemenuTexture from "./SidemenuTexture.vue";
+import SidemenuSettings from "./SidemenuSettings.vue";
+export default {
+    components: {
+        SidemenuBlock,
+        SidemenuTexture,
+        SidemenuSettings
+    },
+    data() {
+        return {
+            tab: ""
+        };
+    }
+};
+</script>
