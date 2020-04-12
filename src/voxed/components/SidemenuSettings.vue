773000b83cb39b01ca76256eacd06d56235db6cd
Blaž Pečnik <blaz@easistent.com>
2020-04-12 08:13:48 +0200

    ...

--- src/voxed/components/SidemenuSettings.vue
+++ src/voxed/components/SidemenuSettings.vue
@@ -1,3 +1,28 @@
 <template>
-    <div>Settings</div>
+    <div>
+         <v-checkbox
+            label="Render wireframe"
+            :input-value="wireframe"
+            @change="toggleWireframe"></v-checkbox>
+    </div>
 </template>
+<script>
+import { Tool_ID } from "../tools/Tool";
+export default {
+    data() {
+        return {
+            items: Object.keys(Tool_ID)
+        };
+    },
+    computed: {
+        wireframe() {
+            return this.$store.state.renderWireframe;
+        }
+    },
+    methods: {
+        toggleWireframe(value) {
+            this.$store.state.renderWireframe = value;
+        }
+    }
+};
+</script>
