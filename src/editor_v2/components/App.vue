183754fc84fb226d941e041a198151859381de70
Blaž Pečnik <blaz@easistent.com>
2020-05-30 12:12:37 +0200

    sidemenu

--- src/editor_v2/components/App.vue
+++ src/editor_v2/components/App.vue
@@ -11,7 +11,7 @@
     </v-app>
 </template>
 <script>
-import Sidemenu from "./Menu.vue";
+import Sidemenu from "./Sidemenu.vue";
 import Viewport from "./Viewport.vue";
 export default {
     components: { Sidemenu, Viewport }
